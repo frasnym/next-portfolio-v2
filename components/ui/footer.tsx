59bbeb46855d90ce36dab81ebbc2095fd625fc8a
FrasNym <frastyawan.nym@gmail.com>
2022-02-16 12:12:00 +0800

    Add media social to footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,15 +1,33 @@
 import type { NextPage } from 'next'
 import styled from "styled-components";
+import { MEDIA_SOCIAL_LINKS } from '../utils/contant';
 
 const Container = styled.footer`
   text-align: right;
   padding: 0.5rem 0 1rem;
+  & a {
+    padding: 0 0.2rem;
+    vertical-align: middle;
+  }
 `
-
 const Footer: NextPage = () => {
   return (
     <Container>
-      <strong>FrasNym</strong> &copy; {new Date().getFullYear()}
+      {
+        MEDIA_SOCIAL_LINKS.map(aIcon => {
+          return (
+            <a
+              key={aIcon.href}
+              href={aIcon.href}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {aIcon.icon}
+            </a>
+          )
+        })
+      }
+      | <b>FrasNym</b> &copy; {new Date().getFullYear()}
     </Container>
   )
 }
